71733d03c82de1d3edb7f8c3a3c336eee18df801
Jayro Salgado <jsalgado@nearsoft.com>
2015-07-07 12:42:51 -0700

    insert and findall

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
+var mongo = {
+  db: 'workshop',
+  col: 'basic',
+  host: 'localhost',
+  port: '27017'
+};
+var url = 'mongodb://'+mongo.host+':'+mongo.port+'/'+mongo.db;
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req,res,next){
+	
+	MongoClient.connect(url, function(err, db) {
+  		//find all customers
+  		var query = {};
+  		db.collection(mongo.col).find(query).sort({firstName:1}).toArray(function(err, docs){
+  			req.list = docs;
+  			next();
+  		});
+	});
+},function(req, res, next) {
+  res.render('index', { title: 'MongoDB CRUD Basics', list: req.list});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+
+/**
+  Mongo client implementation
+*/
+var mongo = {
+  db: 'workshop',
+  col: 'basic',
+  host: 'localhost',
+  port: '27017'
+};
+
+var url = 'mongodb://'+mongo.host+':'+mongo.port+'/'+mongo.db;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+router.post('/insert', function(req, res){
+
+	var body = req.body;
+	console.log(body);
+	// new user logic
+	Object.keys(body).forEach(function(prop) {
+		if(!body[prop]){
+			delete body[prop];	
+		}
+	});
+
+	MongoClient.connect(url, function(err, db) {
+  		console.log("Connected correctly to server");
+  		
+  		db.collection(mongo.col).insert(body, function(err, res){
+  			console.log(res.ops);
+  			db.close();
+  		})
+	});
+
+	res.redirect('/');
+})
+
+
+
+
+
+
 
 module.exports = router;
